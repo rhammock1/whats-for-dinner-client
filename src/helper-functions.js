ffdb69f5646595e0d4b8d60533e89670d8d60077
rhammock1 <rhamm1027@gmail.com>
2020-12-16 11:23:10 -0500

    Little bug fixes

--- src/helper-functions.js
+++ src/helper-functions.js
@@ -2,26 +2,10 @@
 export function findRestaurant(restaurants, restaurantId) {
   
   let restaurant = restaurants.filter(restaurant =>
-    restaurant.id == restaurantId)
+    restaurant.id === parseFloat(restaurantId))
   
   return restaurant[0];
 };
 
-//  export function findRecipe(recipeId) {
-    
-//     return fetch(`${config.API_ENDPOINT}/recipes/${recipeId}`)
-//       .then(res => {
-//         if(!res.ok) {
-//           return res.json().then(e => Promise.reject(e))
-//         }
-//         return res.json()
-//       })
-//       .then(recipe => {
-//         console.log(recipe)
-//          return recipe
-//       })
-//       .catch(error => {
-//       console.error({ error })
-//     })
-//   }
+
 
