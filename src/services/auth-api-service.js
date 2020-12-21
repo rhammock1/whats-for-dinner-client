552716fb82fce0b18f202351d480a60880650855
rhammock1 <rhamm1027@gmail.com>
2020-12-21 15:54:21 -0500

    added api auth services

---
+++ src/services/auth-api-service.js
@@ -0,0 +1,40 @@
+import config from '../config';
+
+const AuthApiService = {
+  postLogin({ user_name, password }) {
+    return fetch(`${config.API_ENDPOINT}/auth/login`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({ user_name, password }),
+    })
+      .then(res => {
+         if (!res.ok) {
+           return res.json().then(e => Promise.reject(e))
+        } else {
+          return res.json()
+        }
+      }
+       
+      )
+  },
+  postUser(user) {
+    return fetch(`${config.API_ENDPOINT}/users`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+      .then( res => {
+        if(!res.ok) {
+          return res.json().then(e => Promise.reject(e))
+        } else {
+          return res.json()
+        }
+      })
+  }
+}
+
+export default AuthApiService;
