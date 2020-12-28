3b17c819a4046bc5c13943cf3293d67be98a421f
rhammock1 <rhamm1027@gmail.com>
2020-12-28 10:35:47 -0500

    allows new users to make an account

---
+++ src/RegistrationForm/RegistrationForm.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import AuthApiService from '../services/AuthApiService';
+
+class RegistrationForm extends React.Component {
+  state = { error: null }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { user_name, first_name, password } = event.target;
+
+    this.setState({ error: null })
+    AuthApiService.postUser( {
+      user_name: user_name.value,
+      first_name: first_name.value,
+      password: password.value,
+    })
+      .then(user => {
+        user_name.value = ''
+        first_name.value = ''
+        password.value = ''
+        this.props.onRegistrationSuccess()
+      })
+      .catch(res => {
+        this.setState({ error: res.error })
+      })
+  }
+
+  render() {
+    const { error } = this.state;
+    return (
+      <form className='registration-form' onSubmit={this.handleSubmit} >
+        <div role='alert'>{error && <p className='red'>{error}</p>}</div>
+        <div className='form-group'>
+          <label htmlFor='user_name'>Username: </label>
+          <input required type='text' id='user_name' name='user_name' />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='first_name'>First Name: </label>
+          <input required type='text' id='first_name' name='first_name' />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password'>Password: </label>
+          <input required type='password' id='password' name='password' />
+        </div>
+        <button type='submit'>Register</button>
+      </form>
+    )
+  }
+}
+
+export default RegistrationForm;
