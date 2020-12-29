abcc5f1fb45271d91678a2b854487f1389ecf400
rhammock1 <rhamm1027@gmail.com>
2020-12-29 17:36:34 -0500

    added beginning of post request

--- src/NewThing/NewThing.js
+++ src/NewThing/NewThing.js
@@ -8,22 +8,30 @@ class NewThing extends React.Component {
   }
 
   handleChange = (event) => {
-    console.log(event.target.value)
     this.setState({ thing: event.target.value })
   }
 
+  handleSubmitThing = event => {
+    event.preventDefault();
+    console.log('hello');
+    // Needs to make a post request to save the new thing with their userId
+  }
+
   render() {
+    const thingCapitalized = this.state.thing.charAt(0).toUpperCase() + this.state.thing.slice(1)
     return (
+      <>
+      <h2>Add New {thingCapitalized}</h2>
       <div className='form-container'>
-        <form className='new-thing-form'>
+        <form className='new-thing-form' onSubmit={event => this.handleSubmitThing(event)}>
           <fieldset>
             <legend>
               Add New:
             </legend>
             <div className='form-group'>
               <label htmlFor='new-type'>Restaurant or Recipe? </label>
-              <select onChange={event => this.handleChange(event)} id='new-type' name='new-type'>
-                <option value={null}>Please Choose one:</option>
+              <select onChange={event => this.handleChange(event)} id='new-type' name='new-type' required>
+                <option value=''>Please Choose one:</option>
                 <option value='restaurant'>Restaurant</option>
                 <option value='recipe'>Recipe</option>
               </select>
@@ -31,9 +39,11 @@ class NewThing extends React.Component {
           {/* {this.state.thing === restaurant
                 ? <div className='form-group'><NewRestaurant /></div>
                 : this.state.thing === recipe ? <div className='form-group'><NewRecipe /></div>} */}
+            <button type='submit'>Add {this.state.thing}</button>
           </fieldset>
         </form>
       </div>
+      </>
         )
   }
 }
