946d5d578e2c4e856e0a5906d264b2230233d275
rhammock1 <rhamm1027@gmail.com>
2020-12-31 11:54:17 -0500

    Favorites works, a couple bugs still

--- src/UserRestaurants/UserRestaurants.js
+++ src/UserRestaurants/UserRestaurants.js
@@ -2,30 +2,124 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import apiService from '../services/api-service';
 
+// TODO:
+//  Favorites becomes undefined on login, need to assign a default value to prevent that
+//  Need to add validation so there wont be duplicates in this.state.favorites
+//  Remove Console.log()
+//  Needs to be refactored to move some things to helper functions or the like. This file is too large
 
 class UserRestaurants extends React.Component {
 
   state = {
     restaurants: [],
+    splicedRestaurants: [],
     added: false,
     favorites: [],
     deleted: false,
+    favoriteRestaurants: [],
   }
 
 
   componentDidMount() {
+    // Makes API calls to get restaurants and favoritees
+    // Should prolly be refactored so it uses context from the APP component instead of making it's own api request?
     const {userId} = this.props.match.params
     apiService.getUsersThings(userId, 'restaurants')
       .then(restaurants => this.setState({restaurants}))
       .catch(error => console.error(error))
     apiService.getUsersThings(userId, 'favorites')
       .then(favorites => this.setState({favorites}))
+      .then(() => this.handleFavorites())
       .catch(error => console.error(error))
-}
+    
+    }
+
+  
+// Removes duplicates from favoritedRestaurants array
+// Compares favorited restaurants with state.restaurants to prevent duplicate rendering
+// 
+  handleFavorites = () => {
+    const favorites = this.state.favorites
+    const favoritedRestaurants = favorites.filter(favorite => {
+      return favorite.what_it_is === 'restaurant'
+    })
+    let removedDoubles = [];
+    for(let i = 0; i < favoritedRestaurants.length; i++) {
+      
+      removedDoubles.push(favoritedRestaurants.filter(favorite => {
+        if (favorite.item_id === favoritedRestaurants[i].item_id) {
+          return favorite
+      } 
+      }))
+    }
+    console.log(removedDoubles)
+    let doubleFree = []
+    removedDoubles.filter(array => {
+      if(array.length < 2) {
+        
+        return doubleFree.push(array[0])
+      }
+    })
+    
+    removedDoubles = removedDoubles.filter(array => {
+      if(array.length >= 2) {
+        return array
+      }
+    })
+  
+    let singleDouble;
+    singleDouble = removedDoubles.map(double => double[0])
+    console.log(singleDouble)
+    const set = new Set(singleDouble)
+    let iterator = set.entries()
+    console.log(set.size)
+    for(let i = 0; i < set.size; i++) {
+      doubleFree.push(iterator.next().value[0])
+    }
+    
+    console.log(doubleFree)
+    const restaurantsInState = [...this.state.restaurants];
+   
+    let filtered = [];
+    let newArray;
+    for(let i = 0; i < doubleFree.length; i++) {
+      
+      filtered.push(restaurantsInState.filter(restaurant => {
+
+        if(restaurant.id === doubleFree[i].item_id) {
+        
+          return restaurant;
+        }
+        }))
+     
+      let itemIds = doubleFree.map(each => each.item_id)
+      console.log('hello', itemIds)
+      newArray = restaurantsInState.filter(restaurant => !itemIds.includes(restaurant.id))
+    
+
+
+    }
+
+
+    let restaurants=[];
+    filtered.map(array => 
+ 
+      array.map(restaurant => 
+        
+        restaurants.push(restaurant)
+      )
+    )
+    console.log(restaurants)
+    
+  this.setState({ favoriteRestaurants: restaurants })
+  this.setState({ splicedRestaurants: newArray })
+
+  }
 
 handleAddToFavorites = event => {
   event.preventDefault();
   const restaurantId = event.target.id;
+
   const {userId} = this.props.match.params
   const newFavorite = {
     what_it_is: 'restaurant',
@@ -33,9 +127,16 @@ handleAddToFavorites = event => {
     item_id: restaurantId
   }
   
-  console.log(newFavorite)
+
   apiService.postNewFavorite(userId, newFavorite)
     .then(() => {this.setState({ added: true })})
+    .then(() => {
+      apiService.getUsersThings(userId, 'favorites')
+      .then(favorites => {
+         this.setState({favorites})})
+      .then(async () => await this.handleFavorites())
+      .catch(error => console.error(error))
+    })
     .then(() => {
       setTimeout(
         () => {this.setState({ added: false})},
@@ -43,11 +144,12 @@ handleAddToFavorites = event => {
       )
     })
     .catch(error => console.error(error))
+    
 }
 
 handleRemoveFromFavorites = event => {
   event.preventDefault();
-  console.log('hello');
+  
   const {userId} = this.props.match.params;
   const favoritedRestaurants = this.state.favorites.filter(favorite => {
       return favorite.what_it_is === 'restaurant'
@@ -65,51 +167,25 @@ handleRemoveFromFavorites = event => {
   apiService.deleteFavorite(userId, favoriteId)
     .then(() => {this.setState({ deleted: true })})
     .then(() => {
+      apiService.getUsersThings(userId, 'favorites')
+      .then(favorites => this.setState({favorites}))
+      .then(async () => await this.handleFavorites())
+      .catch(error => console.error(error)) 
+    })
+    .then(() => {
       setTimeout(
         () => {this.setState({ deleted: false})},
         5000
       )
     })
     .catch(error => console.error(error))
+  
 
 }
   render() {
-    const favorites = this.state.favorites
-    const favoritedRestaurants = favorites.filter(favorite => {
-      return favorite.what_it_is === 'restaurant'
-    })
-    // favoritedRestaurants.sort((a, b) => {
-    //   return a.item_id - b.item_id;
-    // })
-   
-
-    const restaurantsInState = this.state.restaurants;
-
-   
-    let filtered = [];
-
-    for(let i = 0; i < favoritedRestaurants.length; i++) {
+    const restaurants = this.state.favoriteRestaurants;
+    const restaurantsInState = this.state.splicedRestaurants || [{}];
     
-      filtered.push(restaurantsInState.filter(restaurant => restaurant.id === favoritedRestaurants[i].item_id))
-      let restaurantIndex = restaurantsInState.findIndex(restaurant => {
-        return restaurant.id === favoritedRestaurants[i].item_id
-      })
-      restaurantsInState.splice(restaurantIndex, 1);
-      
-
-    }
-      console.log(filtered)
-
-
-    let restaurants=[];
-    filtered.map(array => 
- 
-      array.map(restaurant => 
-        
-        restaurants.push(restaurant)
-      )
-    )
-
     
     return (
       <section className='user-restaurants'>
@@ -124,12 +200,12 @@ handleRemoveFromFavorites = event => {
                   <p>Remove from favorites</p>
                   <img alt='button-to-remove-from-favorite' id={restaurant.id} onClick={event => this.handleRemoveFromFavorites(event)}src="https://img.icons8.com/office/16/000000/add-to-favorites--v2.png"/>
                 </div>
-                {/* {this.state.deleted
+                {this.state.deleted
                   ? <div className='added'>
                       <p>Successfully deleted from favorites</p>
                     </div>
                   : null
-                } */}
+                }
               </div>
             )
           })}
