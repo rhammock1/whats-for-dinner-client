fb5f8ebf8269dfbe67d2cf3749c90bc4f9d4969e
rhammock1 <rhamm1027@gmail.com>
2020-12-31 20:14:07 -0500

    added snapshot and cleaned up the tests

--- src/Components/LoginForm/LoginForm.test.js
+++ src/Components/LoginForm/LoginForm.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
-import NewThing from './NewThing';
+import LoginForm from './LoginForm';
 
 
 describe('Form component for user to add new thing', () => {
   it('renders the complete form', () => {
-    const wrapper = shallow(<NewThing />)
+    const wrapper = shallow(<LoginForm />)
     expect(toJson(wrapper)).toMatchSnapshot()
   })
 })
--- src/Components/RegistrationForm/RegistrationForm.test.js
+++ src/Components/RegistrationForm/RegistrationForm.test.js
@@ -2,10 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import RegistrationForm from './RegistrationForm';
-// import Enzyme from 'enzyme'
-// import Adapter from 'enzyme-adapter-react-16'
 
-// Enzyme.configure({ adapter: new Adapter() });
 
 describe('Form component for user to add new thing', () => {
   it('renders the complete form', () => {
