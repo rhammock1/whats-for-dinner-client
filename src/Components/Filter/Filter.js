2eefb712b9711336b76cdc6871da67a17adf7be2
rhammock1 <rhamm1027@gmail.com>
2021-01-01 11:57:16 -0500

    Added some styling updated filter require

--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -10,11 +10,11 @@ class Filter extends React.Component {
     return (
       <form onSubmit={e => this.context.handleWheelOptions(e)}onChange={this.props.handleChange} className='filters'>
       <fieldset>
-        <legend>What are you looking for?</legend>
+        <legend><h2>What are you looking for?</h2></legend>
         <div className='form-group'>
         <label htmlFor='inOrOut'>Restaurants or Recipes?</label>
         <select id='inOrOut' name='inOrOut' required>
-          <option value='null'>Choose One:</option>
+          <option value=''>Choose One:</option>
           <option value='restaurants'>Restaurants</option>
           <option value='recipes'>Recipes</option>
           <option value='both'>Either</option>
@@ -22,7 +22,7 @@ class Filter extends React.Component {
         </div>
         {(this.context.inOrOut === 'restaurants' || this.context.inOrOut === 'both')
           ? <div className='form-group'><label htmlFor='style'>Style: </label> <select id='style' name='style' required>
-            <option value='null'></option>
+            <option value=''></option>
             <option value='chain'>Chain</option>
             <option value='local'>Local</option>
           </select> </div>
