37f2bf0b807660b1dbf66eb92a62f164bb124bf2
rhammock1 <rhamm1027@gmail.com>
2021-01-06 16:37:46 -0500

    All tests working

--- src/Components/NewThing/NewRecipe/NewRecipe.test.js
+++ src/Components/NewThing/NewRecipe/NewRecipe.test.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import NewRecipe from './NewRecipe';
 import { BrowserRouter } from 'react-router-dom';
 
+const ingredients = [
+  { id: 1, amount: 2, unit: 'cups', ingredient: 'carrot'},
+]
+
 describe('New Recipe Form section component', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<BrowserRouter><NewRecipe />
+    ReactDOM.render(<BrowserRouter><NewRecipe ingredients={ingredients} />
     </BrowserRouter>, div)
   })
 })
