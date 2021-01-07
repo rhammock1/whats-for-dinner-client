70f1c5072d8855418cae47e40a5a2b8c2fb780af
rhammock1 <rhamm1027@gmail.com>
2021-01-07 12:17:14 -0500

    Client side is done for now

--- src/Components/Popup/Popup.js
+++ src/Components/Popup/Popup.js
@@ -24,6 +24,11 @@ function Popup(props) {
         You can choose between viewing restaurants, recipes, or both
         in the filter below. Then spin the wheel to see what&apos;s for dinner!
       </p>
+      <p>
+        If you are logged in and have added favorites, 3 of those favorites will
+        always appear on the wheel. If you have added more than 3 local restaurants
+        then a new style option will appear to filter by local restaurants.
+      </p>
       <button type="button" onClick={handleClear}>Clear</button>
     </div>
 
